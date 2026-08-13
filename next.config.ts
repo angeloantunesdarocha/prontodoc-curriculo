42a72602e0522d29a1d0a6396ca91e8f1595bba3
Ângelo Antunes da Rocha <angeloantunesdarocha@gmail.com>
2026-08-13 07:59:54 -0300

    Adiciona headers básicos de segurança

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,26 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {};
+const nextConfig: NextConfig = {
+  async headers() {
+    return [
+      {
+        source: "/(.*)",
+        headers: [
+          { key: "X-Content-Type-Options", value: "nosniff" },
+          { key: "X-Frame-Options", value: "DENY" },
+          { key: "Referrer-Policy", value: "no-referrer" },
+          {
+            key: "Permissions-Policy",
+            value: "camera=(self), microphone=(self), geolocation=(), payment=(self)",
+          },
+          {
+            key: "Strict-Transport-Security",
+            value: "max-age=31536000; includeSubDomains; preload",
+          },
+        ],
+      },
+    ];
+  },
+};
 
 export default nextConfig;
